cf1b5eedc23a1fe28bcc7217a588d1b6ff990731
Marcin Borkowski <marcin.d.borkowski@gmail.com>
2024-12-27 22:52:03 +0100

    Init

---
+++ src/test/java/com/example/training_plans/TrainingPlansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.training_plans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingPlansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
